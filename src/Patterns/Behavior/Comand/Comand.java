8380545bcb78afcaf654f42fa25621cb05fc1f90
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-29 23:22:47 +0500

    done other patterns

---
+++ src/Patterns/Behavior/Comand/Comand.java
@@ -0,0 +1,6 @@
+package Patterns.Behavior.Comand;
+
+public class Comand {
+
+    // MVP то же самое чем - то смахивает на Proxy
+}
---
+++ src/Patterns/Behavior/Tenplate_Method/TemplateMethod.java
@@ -0,0 +1,6 @@
+package Patterns.Behavior.Tenplate_Method;
+
+public class TemplateMethod {
+
+    // Builder только по алгоритмам
+}
