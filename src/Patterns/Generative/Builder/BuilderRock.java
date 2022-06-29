5bf40487437fc7faeeb885ca19a705c37b2c2e56
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-29 13:38:59 +0500

    done builder pattern

--- src/Patterns/Generative/Builder/BuilderRock.java
+++ src/Patterns/Generative/Builder/BuilderRock.java
@@ -1,4 +1,60 @@
 package Patterns.Generative.Builder;
 
-public class BuilderRock {
+public class BuilderRock implements Builder {
+
+    private HouseRock houseRock;
+
+    @Override
+    public void reset() {
+
+        houseRock = new HouseRock();
+
+    }
+
+    @Override
+    public void setWall() {
+
+        //houseRock.setWall("Rock");
+
+        System.out.println("Set Rock Wall");
+
+    }
+
+    @Override
+    public void setRoof() {
+
+       // houseRock.setRoof("Rock Roof");
+
+        System.out.println("Set Roof");
+
+    }
+
+    @Override
+    public void setFundamental() {
+
+       // houseRock.setFundamental(1);
+
+        System.out.println("Set Foundamental");
+
+    }
+
+    @Override
+    public void setDoor() {
+
+        //houseRock.setDoor("Rock door");
+
+        System.out.println("Set Door");
+
+    }
+
+    @Override
+    public void setWindow() {
+
+        //houseRock.setWindow("Rock window");
+
+        System.out.println("Set Rock Window");
+
+    }
+
+    public HouseRock getResult() { return houseRock; }
 }
